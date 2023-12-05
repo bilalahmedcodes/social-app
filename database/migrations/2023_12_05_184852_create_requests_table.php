8e4da281b837be722e21cc7644f8b0555b84054f
Bilal Ahmed <bilalahmedcodes@gmail.com>
2023-12-06 00:50:49 +0500

    auth-integrated

--- database/migrations/2023_12_05_184852_create_requests_table.php
+++ database/migrations/2023_12_05_184852_create_requests_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('requests', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('sender_id');
+            $table->integer('reciever_id');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('requests');
     }
 };
